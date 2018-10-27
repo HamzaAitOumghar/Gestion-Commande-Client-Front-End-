5f6e2797310c1f8768e8fcfb0a2f31fae1d8e2a0
HAMZA AIT OUMGHAR <hamwac5@yahoo.fr>
2018-10-28 00:05:18 +0100

    Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routes} from './app.routing';
 
+import { SidebarModule } from 'ng-sidebar';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
